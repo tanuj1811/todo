34ef7d54b3b3d6515d45b5be8cb8a9e617afe1fb
Tanuj Sharma <tanujsharma56673@gmail.com>
2022-04-23 19:46:59 +0530

    Todo List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import MobileTearSheet from './Components/MoblieTearSheet'
+import { LIST } from './shared/object'
+import './app.scss'
+import { Card } from './Components/'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faArrowRotateRight,
+  faTrashArrowUp,
+} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [completedTask, setCompletedTask] = useState([])
+  const [unCompletedTask, setUnCompletedTask] = useState([])
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    let compTask = LIST.filter((task) => task.isDone)
+    setCompletedTask(compTask)
+    let unCompTask = LIST.filter((task) => !task.isDone)
+    setUnCompletedTask(unCompTask)
+  }, [])
+
+  const taskHandler = (task) => {
+    console.log('clicked' + task.isDone)
+    const data = {
+      _id: task._id,
+      todo: task.todo,
+      isDone: task.isDone ? false : true,
+    }
+    if (!task.isDone) {
+      setCompletedTask((oldItems) => [...oldItems, data])
+      let data2 = unCompletedTask.filter((t) => t._id !== data._id)
+      setUnCompletedTask(data2)
+    } else {
+      setUnCompletedTask((oldItems) => [data, ...oldItems])
+      let data2 = completedTask.filter((t) => t._id !== data._id)
+      setCompletedTask(data2)
+    }
+  }
+
+  const inputHandler = (e) => {
+    if (e.key === 'Enter') {
+      const data = {
+        _id: 'fsfsfsf' + input,
+        todo: input,
+        isDone: false,
+      }
+      setUnCompletedTask((oldtasks) => [data, ...oldtasks])
+      console.log(unCompletedTask)
+      setInput('')
+    }
+  }
+
+  const resetHandler = () => {
+    setCompletedTask([])
+    setUnCompletedTask([])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Todo List</h1>
       </header>
+      <div className="container">
+        <input
+          placeholder="Add task"
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={inputHandler}
+          value={input}
+        />
+        <MobileTearSheet>
+          <div className="features">
+            <span>
+              <FontAwesomeIcon onClick={resetHandler} icon={faTrashArrowUp} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faArrowRotateRight} />
+            </span>
+          </div>
+          <h4>Things to do</h4>
+          <ul>
+            {unCompletedTask &&
+              unCompletedTask.map((task) => (
+                <Card onClick={() => taskHandler(task)}>{task.todo}</Card>
+              ))}
+            {unCompletedTask.length === 0 && <h3>Empty</h3>}
+          </ul>
+          <h4>{completedTask.length > 0 && 'History '}</h4>
+          <ul>
+            {completedTask &&
+              completedTask.map((task) => (
+                <Card onClick={() => taskHandler(task)}>{task.todo}</Card>
+              ))}
+          </ul>
+        </MobileTearSheet>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/shared/object.js
@@ -0,0 +1,19 @@
+export const LIST = [
+  {
+    _id: 'test1',
+    todo: 'test-1',
+    isDone: false,
+  },
+  {
+    _id: 'test2',
+    todo:
+      'test-2 - > big test : Builds the app for production to the `build` folder. It correctly bundles React in production mode and optimizes the build for the best performance.',
+    isDone: false,
+  },
+  {
+    _id: 'test3',
+    todo:
+      'test-3 - > big test : Builds the app for production to the `build` folder. It correctly bundles React in production mode and optimizes the build for the best performance.',
+    isDone: false,
+  },
+]
