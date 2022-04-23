a7eb8d96f5d6eb5ef347a2e30dff7652d191c641
Tanuj Sharma <tanujsharma56673@gmail.com>
2022-04-23 21:25:20 +0530

    edited package.json for deploying to github pages

--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import './card.scss'
 const Card = (props) => {
-  const handler = () => {
-    console.log('fsfksfslkfda')
-  }
-
   return (
     <div className="card" onClick={props.onClick}>
       {props.children}
